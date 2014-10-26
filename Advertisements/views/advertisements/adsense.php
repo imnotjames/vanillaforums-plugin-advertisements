d01cf5a2a85c14c3f0d34e2bdfaff9dc6b481296
James Ward <james@notjam.es>
2014-10-26 18:36:22 -0400

    set orientation correctly

--- Advertisements/views/advertisements/adsense.php
+++ Advertisements/views/advertisements/adsense.php
@@ -2,6 +2,12 @@
 
 <?php $Width = empty($Configuration->GetWidth()) ? 'auto' : intval($Configuration->GetWidth()) . 'px'; ?>
 <?php $Height = empty($Configuration->GetHeight()) ? 'auto' : intval($Configuration->GetHeight()) . 'px'; ?>
+<?php $Orientations = [
+	AdvertisementsPlugin_Configuration::ORIENTATION_VERTICAL => 'vertical',
+	AdvertisementsPlugin_Configuration::ORIENTATION_HORIZONTAL => 'horizontal',
+	AdvertisementsPlugin_Configuration::ORIENTATION_OTHER => 'square'
+	];
+?>
 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins
@@ -13,7 +19,9 @@
 		"
 	data-ad-client="<?= htmlentities($Configuration->getPublisherIdentifier()); ?>"
 	data-ad-slot="<?= htmlentities($Configuration->getAdvertisementIdentifier()); ?>"
-	data-ad-format="<?= htmlentities($Configuration->getOrientation()) ? 'vertical' : 'horizontal'; ?>"
+	<?php if (!$Configuration->IsAutomaticOrientation()): ?>
+	data-ad-format="<?= htmlentities($Orientations[$Configuration->getOrientation()]); ?>"
+	<?php endif; ?>
 >
 	<?= $Configuration->getDisabledAdsText(); ?>
 </ins>
