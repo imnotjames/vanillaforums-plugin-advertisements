3bb027e1a0481d8a2306532227c5c52ad99f8400
James Ward <james@notjam.es>
2014-10-29 22:53:32 -0400

    use data-ad-format=auto for automatic types

--- Advertisements/views/advertisements/adsense.php
+++ Advertisements/views/advertisements/adsense.php
@@ -3,6 +3,7 @@
 <?php $Width = $Configuration->GetWidth() === 0 ? 'auto' : $Configuration->GetWidth() . 'px'; ?>
 <?php $Height = $Configuration->GetHeight() === 0 ? 'auto' : $Configuration->GetHeight() . 'px'; ?>
 <?php $Orientations = [
+	AdvertisementsPlugin_Configuration::ORIENTATION_AUTOMATIC => 'auto',
 	AdvertisementsPlugin_Configuration::ORIENTATION_VERTICAL => 'vertical',
 	AdvertisementsPlugin_Configuration::ORIENTATION_HORIZONTAL => 'horizontal',
 	AdvertisementsPlugin_Configuration::ORIENTATION_OTHER => 'square'
@@ -19,9 +20,7 @@
 		"
 	data-ad-client="<?= htmlentities($Configuration->getPublisherIdentifier()); ?>"
 	data-ad-slot="<?= htmlentities($Configuration->getAdvertisementIdentifier()); ?>"
-	<?php if (!$Configuration->IsAutomaticOrientation()): ?>
 	data-ad-format="<?= htmlentities($Orientations[$Configuration->getOrientation()]); ?>"
-	<?php endif; ?>
 >
 	<?= $Configuration->getDisabledAdsText(); ?>
 </ins>
